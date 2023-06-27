a6972bdba76934b7c79201a02527adb7f2560701
user.email <aromito7@gmail.com>
2023-06-27 14:16:25 -0400

    refactored the drop buttons so they display the next piece while hovering

--- react-app/src/components/GamePage/index.js
+++ react-app/src/components/GamePage/index.js
@@ -7,7 +7,8 @@ import { useState, useEffect } from "react";
 const ROWS = 6;
 const COLS = 7;
 
-let display = Array(ROWS).fill().map(col => Array(COLS).fill(' '))
+
+const display = Array(ROWS).fill().map(col => Array(COLS).fill(' '))
 let board
 let active = Array(COLS).fill(5)
 
@@ -24,104 +25,12 @@ let previousHover
 //   opponent = new Player(1, 2)
 // }
 
-function resetGame(){
-  document.querySelectorAll(".dropButton").forEach(child => child.style.visibility = "visible")
-  document.getElementById("endGame").innerText = ""
-  // board = Array(ROWS).fill().map(col => Array(COLS).fill(0))
-  // active = Array(COLS).fill(5)
-//   const grid = document.getElementById("grid");
-//   for (let y = 0; y < ROWS; y++) {
-//     for (let x = 0; x < COLS; x++) {
-//       const current = document.getElementById(`${x},${y}`);
-//       current.setAttribute("class", "empty");
-//     }
-//   }
-}
 
-function clearScreen() {
-  for (let y = 0; y < ROWS; y++) {
-    for (let x = 0; x < COLS; x++) {
-      const current = document.getElementById(`${x},${y}`);
-      current.setAttribute("class", "dead");
-    }
-  }
-}
-
-function startGame() {
-  const start = document.getElementById("start");
-  start.innerHTML = "NEW GAME";
-  gameOver = false
-  resetGame()
-}
-
-
-// function initButtons() {
-//   //const clear = document.getElementById("clear");
-//   //const next = document.getElementById("next");
-//   const start = document.getElementById("start");
-//   start.addEventListener("click", () => startGame());
-//   //clear.addEventListener("click", () => clearScreen());
-//   //  next.addEventListener("click", () => nextState());
-// }
-
-function checkForWin([x, y], board, player){
-  const chain = Player.longestChainAtLocation([x, y], board, player)
-  //console.log(chain, [x,y], board, player)
-
-  return chain >= 3? 1 : 0
-}
-
-function endGame(player){
-  gameOver = true
-  const dropButtons = document.querySelectorAll(".dropButton")
-  const endMessage = document.getElementById("endGame")
-  endMessage.innerText = player === 2 ? "You Lose!" : "You Win"
-  //endMessage.setAttribute("display", "visible")
-  dropButtons.forEach(child => child.style.visibility = "hidden")
-}
-
-const pressButton = function(player, column){
-  //console.log(column, active[column], board)
-  //board[active[column]][column].setAttribute("class", player === 1 ? "black" : "red")
-  return () => {
-    if(gameOver) return
-    let recentMove = columnDrop(player, column)
-    if(checkForWin(recentMove, board, 1)){
-      endGame(1)
-      return
-    }
-    recentMove = columnDrop(currentPlayer, opponent.decide(board, active))
-    if(checkForWin(recentMove, board, 2)){
-      endGame(2)
-      return
-    }
-  }
-}
-
-const columnDrop = function(player, column){
-  console.log(`Column drop.  Player: ${player}, Column: ${column}`)
-  //if(currentPlayer && currentPlayer !== player) return
-  const [y, x] = [active[column], column]
-  //console.log(`Hello Player ${player}.  You've selected ${x}, ${y}`)
-  // display[y][x] = player === 1 ? "B" : "R"
-  previousHover = null
-  currentPlayer = 3 - currentPlayer
-
-  active[column]--
-}
-
-const columnHover = function(player, column){
-  console.log(player, column)
-  return () => {
-    if(gameOver) return
-    const [y , x] = [active[column], column]
-    display[y][x] = 'G'
-  }
-}
 
 
 const Grid = () => {
-  //console.log("Grid: ")
+  console.log("Rerendering Grid")
+  console.log(display)
   return (
     <div id="grid">
       {
@@ -139,7 +48,8 @@ const Grid = () => {
 
 const Square = ({val, x, y}) => {
   let color;
-  console.log(x, y, val)
+  // console.log("Rerendering square:")
+  // console.log(x, y, val)
   switch(val){
     case('G'):
       color = "grey"
@@ -167,8 +77,84 @@ const Square = ({val, x, y}) => {
 }
 
 const GameBoard = () => {
-  const [colors, setColors] = useState([])
-  let display = Array(ROWS).fill().map(col => Array(COLS).fill(' '))
+  const [currentPlayer, setCurrentPlayer] = useState(1)
+
+
+  function resetGame(){
+    console.log("Resetting Game...")
+    for(let y in display){
+      display[y] = Array(COLS).fill(' ')
+    }
+    active = Array(COLS).fill(5)
+    setCurrentPlayer(1)
+  }
+
+
+  function startGame() {
+    console.log("Start Game...")
+    const start = document.getElementById("start");
+    start.innerHTML = "NEW GAME";
+    gameOver = false
+    resetGame()
+  }
+
+
+
+
+  function checkForWin([x, y], board, player){
+    const chain = Player.longestChainAtLocation([x, y], board, player)
+
+    return chain >= 3? 1 : 0
+  }
+
+  function endGame(player){
+    gameOver = true
+    const dropButtons = document.querySelectorAll(".dropButton")
+    const endMessage = document.getElementById("endGame")
+    endMessage.innerText = player === 2 ? "You Lose!" : "You Win"
+    //endMessage.setAttribute("display", "visible")
+    dropButtons.forEach(child => child.style.visibility = "hidden")
+  }
+
+  const pressButton = function(player, column){
+    //console.log(column, active[column], board)
+    //board[active[column]][column].setAttribute("class", player === 1 ? "black" : "red")
+    return () => {
+      if(gameOver) return
+      let recentMove = columnDrop(player, column)
+      if(checkForWin(recentMove, board, 1)){
+        endGame(1)
+        return
+      }
+      recentMove = columnDrop(currentPlayer, opponent.decide(board, active))
+      if(checkForWin(recentMove, board, 2)){
+        endGame(2)
+        return
+      }
+    }
+  }
+
+  const columnDrop = function(player, column){
+    console.log(`Column drop.  Player: ${player}, Column: ${column}`)
+    //if(currentPlayer && currentPlayer !== player) return
+    const [y, x] = [active[column], column]
+    console.log(`Hello Player ${player}.  You've selected ${x}, ${y}`)
+    display[y][x] = player === 1 ? "B" : "R"
+    previousHover = null
+    setCurrentPlayer(3 - currentPlayer)
+
+    return[column, active[column]--]
+  }
+
+  const columnHover = function(player, column){
+    console.log(player, column)
+    return () => {
+      if(gameOver) return
+      const [y , x] = [active[column], column]
+      display[y][x] = 'G'
+    }
+  }
+
   return (
     <div id="container">
       <h1>Connect 4</h1>
@@ -180,7 +166,7 @@ const GameBoard = () => {
             return(
               <button
               key={x}
-              class="dropButton"
+              class={`dropButton ${currentPlayer === 1? 'hoverBlack' : 'hoverRed'}`}
               onClick={e => columnDrop(1, x)}
               onmouseover={e => columnHover(1, x)}
                 />
