125bccc8cf7ebbcd0e3246c56f7ee94767164e19
user.email <aromito7@gmail.com>
2023-10-16 01:06:58 -0400

    updated .env

--- app/__init__.py
+++ app/__init__.py
@@ -23,8 +23,7 @@ app.cli.add_command(seed_commands)
 app.config.update(
     SECRET_KEY = os.environ.get('SECRET_KEY'),
     SQLALCHEMY_TRACK_MODIFICATIONS = False,
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
-        'DATABASE_URL').replace('postgres://', 'postgresql://'),
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace('postgres://', 'postgresql://'),
     SQLALCHEMY_ECHO = True
 )
 app.register_blueprint(bot_routes, url_prefix='/api/bot')
